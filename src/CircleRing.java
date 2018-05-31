4f27e8d9ed4b1bacdd7d14c270b05c6e044e74cb
echobin23 <league@iMac.attlocal.net>
2018-05-30 19:58:43 -0700

    Goofy Names, Guessing Pi, Circle Rings

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,20 +9,21 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+Robot Bob = new Robot();
 
        //  2. Set your robot’s position to x=150 and y=200
-
+Bob.setX(150);
+Bob.setY(200);
         // 3. Put the robot's pen down
-
+Bob.penDown();
         // 4. Set the robot’s speed to 10
-
+Bob.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
-
+for(int i=0;i<359;i++) {
 	        // 6. Move the robot 3 pixels
-
+Bob.move(3);
        //  7. Turn the robot 1 degree
-
+Bob.turn(1);
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
@@ -30,5 +31,6 @@ public class CircleRing {
 				// 10. Move the robot 1 pixel
 
 				// 11. Turn the robot 1 degree
+}
 }
 }
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,39 +1,43 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
  */
 
 import javax.swing.JOptionPane;
+
 //
 public class GoofyNames {
-	public static void main(String[] args) {//temp
+	public static void main(String[] args) {// temp
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-String answer = JOptionPane.showInputDialog("What is your name?");
+		String answer = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-System.out.println( answer.toUpperCase());
+		// Run your program to see that this works.
+		System.out.println(answer.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-for(int i = 0; i < 4; i++) {
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < answer.length(); i++) {
 			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-String ch1 = 	answer.substring(i,i+1);	
+			// using .substring(start, end)
+			// HINT: replace 'start' and 'end' to get String with character at i
+			String ch1 = answer.substring(i, i + 1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+			// and lower case for ODD characters.
 
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-if(i%2==0) {
-	System.out.println(answer.toUpperCase());
-}
-if(i%2==1) {
-	System.out.println(answer.toLowerCase());
-}
-}
+			// 6. ADD this String (containing 1 char) to the goofyName String
+			if (i % 2 == 0) {
+				ch1 = (ch1.toUpperCase());
+			}
+			if (i % 2 == 1) {
+				ch1 = (ch1.toLowerCase());
+			}
+			goofyName= goofyName+ch1;
+		}
+
 		// 7. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null, goofyName);
 
 	}
 }
-
