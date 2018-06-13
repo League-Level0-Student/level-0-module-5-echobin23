6dff8434f8c198c082c9a9e888ec440be95a9f3a
echobin23 <league@iMac.attlocal.net>
2018-06-12 18:29:23 -0700

    For loop Gauntlet IP

--- src/extra/ForLoopGauntlet.java
+++ src/extra/ForLoopGauntlet.java
@@ -2,7 +2,7 @@ package extra;
 
 public class ForLoopGauntlet {
 	public static void main(String[] args) {
-		for (int i = 0; i < 101; i++) {
+/*		for (int i = 0; i < 101; i++) {
 			System.out.println(i);
 		}
 		for (int c = 100; c > 0; c--) {
@@ -31,7 +31,28 @@ public class ForLoopGauntlet {
 				System.out.println(s);
 			}
 		}
-		for (int b = 0; b < 13; b++) {
+		int year = 2005;
+		for (int b = 0; b < 14; b++) {
+			System.out.print(+year+b);
+			System.out.println(" I was "+b);
+		}*/
+		
+		for(int loop =0; loop <3; loop++) {
+           for(int two = 0; two < 3; two++) {
+        	   System.out.println(loop+" "+two);
+           }
+		}
+		for(int nest = 1; nest < 8; nest+=3) {
+			for(int nest2 = nest; nest2 < nest+3; nest2++) {
+				System.out.print(nest2);
+			}
+			System.out.println();
+		}
+		for(int outl=1; outl<101; outl++) {
+			for(int innerl=outl; innerl<outl+1; innerl++) {
+				System.out.println(innerl);
+				
+			}
 		}
 	}
 }
